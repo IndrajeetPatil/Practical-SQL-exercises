7dcea27743b1290719931f5ac58d83c6f38a04b4
Indrajeet Patil <patilindrajeet.science@gmail.com>
2021-11-14 18:26:57 +0100

    add rest of the files

---
+++ exercises/Chapter-13.sql
@@ -0,0 +1,7 @@
+--------------------------------------------------------------
+-- Practical SQL: A Beginner's Guide to Storytelling with Data
+-- by Anthony DeBarros
+-- 1st Edition
+-- Chapter 13 "Try It Yourself" Exercises
+--------------------------------------------------------------
+--
---
+++ exercises/Chapter-16.sql
@@ -0,0 +1,7 @@
+--------------------------------------------------------------
+-- Practical SQL: A Beginner's Guide to Storytelling with Data
+-- by Anthony DeBarros
+-- 1st Edition
+-- Chapter 16 "Try It Yourself" Exercises
+--------------------------------------------------------------
+--
