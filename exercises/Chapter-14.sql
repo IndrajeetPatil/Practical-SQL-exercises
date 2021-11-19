c5722ddf1fcb9b5572455e20ade43e3fb2ec129b
Indrajeet Patil <patilindrajeet.science@gmail.com>
2021-11-19 18:03:00 +0100

    formatting

--- exercises/Chapter-14.sql
+++ exercises/Chapter-14.sql
@@ -4,15 +4,20 @@
 -- 1st Edition
 -- Chapter 14 "Try It Yourself" Exercises
 --------------------------------------------------------------
+
+
 ---------
 -- Q1 --
 ---------
+
+
 -- area of each state in square miles
+
 SELECT
     statefp10,
     sum(
         round(
-            (ST_Area(geom :: geography) / 2589988.110336) :: numeric,
+            (ST_Area(geom::geography) / 2589988.110336)::numeric,
             2
         )
     ) AS square_miles
@@ -25,11 +30,12 @@ ORDER BY
 
 -- How many states are bigger than Yukon-Koyukuk?
 -- 3 states (statefp10: 02, 06, 48)
+
 SELECT
     statefp10,
     sum(
         round(
-            (ST_Area(geom :: geography) / 2589988.110336) :: numeric,
+            (ST_Area(geom::geography) / 2589988.110336)::numeric,
             2
         )
     ) AS square_miles
@@ -39,10 +45,10 @@ GROUP BY
     statefp10
 HAVING
     sum(
-        (ST_Area(geom :: geography) / 2589988.110336) :: numeric
+        (ST_Area(geom::geography) / 2589988.110336)::numeric
     ) > (
         SELECT
-            (ST_Area(geom :: geography) / 2589988.110336) :: numeric
+            (ST_Area(geom::geography) / 2589988.110336)::numeric
         FROM
             us_counties_2010_shp
         WHERE
@@ -51,10 +57,14 @@ HAVING
 ORDER BY
     square_miles DESC;
 
+
 ---------
 -- Q2 --
 ---------
+
+
 -- extracting lat/long info
+
 SELECT
     market_name,
     street,
@@ -72,26 +82,46 @@ WHERE
 -- it's 405.38 miles
 -- Google shows this distance to be > 1000 miles, so something is wrong here
 -- https://www.google.com/maps/dir/1701+West+Ash+Street,+Columbia,+Missouri,+USA/9700+Argyle+Forest+Blvd,+Jacksonville,+FL+32222,+USA/@34.5090007,-91.5877959,6z/data=!3m1!4b1!4m15!4m14!1m5!1m1!1s0x87dcb61894dab121:0x4493302dd6707e2f!2m2!1d-92.3627!2d38.95749!1m5!1m1!1s0x88e5c19afa6ffb09:0x3789f4b88b06c4ff!2m2!1d-81.8312553!2d30.1950797!3e0!4e1
+
 SELECT
     round(
         ST_Distance(
             ST_GeogFromText('POINT(38.9586090 -92.3636690)'),
             ST_GeogFromText('POINT(30.1942000 -81.8335000)')
-        ) :: numeric / 1609.344,
+        )::numeric / 1609.344,
         2
     ) AS oakleaf_to_columbia;
+
 
 ---------
 -- Q3 --
 ---------
--- cast column geom from geometry type to geography
-UPDATE
-    us_counties_2010_shp
-SET
-    geom = ST_SetSRID(geom, 4326) :: geography;
 
-select
-    namelsad10,
-    geom :: geography
-from
-    us_counties_2010_shp;
+
+-- cast column geom from geometry type to geography
+-- create two CTEs, one for US county
+
+WITH us_counties AS (
+    SELECT
+        namelsad10,
+        ST_SetSRID(geom::geography, 4326) AS "geom"
+    FROM
+        us_counties_2010_shp
+),
+markets AS (
+    SELECT
+        *
+    FROM
+        farmers_markets
+    WHERE
+        county IS NULL
+)
+SELECT
+    markets.county,
+    us_counties.namelsad10,
+    us_counties.geom
+FROM
+    markets
+    JOIN us_counties ON ST_Intersects(markets.geog_point, us_counties.geom)
+ORDER BY
+    markets.fmid;
--- exercises/Chapter-16.sql
+++ exercises/Chapter-16.sql
@@ -4,4 +4,9 @@
 -- 1st Edition
 -- Chapter 16 "Try It Yourself" Exercises
 --------------------------------------------------------------
---
+
+
+/*
+ The exercise is redoing any of the prior analysis in Command prompt using psql
+ So nothing extra needs to be done here
+ */
